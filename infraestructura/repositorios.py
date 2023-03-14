5ec9f9da2eaeb525d83cb842c2a45256dbcfd406
Harold Funeme <h.funeme@uniandes.edu.co>
2023-03-14 09:13:06 -0500

    🐧 DB and ARQ HEXAGONAL FULL

--- infraestructura/repositorios.py
+++ infraestructura/repositorios.py
@@ -1,9 +1,8 @@
 from ast import List
 from config.db import db
 from dominio.repositorios import RepositorioOrdenes, RepositorioProveedores
-from infraestructura.dto import Orden, Item
+from .dto import Orden, Item
 from dominio.fabricas import FabricaOrden
-from .dto import Orden as OrdenDTO
 from .mapeadores import MapeadorOrden
 from uuid import UUID
 
@@ -50,35 +49,17 @@ class RepositorioOrdenesSQLite(RepositorioOrdenes):
         print("RepositorioOrdenesSQLite obj para agregar DB: ", orden)
         print("\n")
    
-    
-        # items: Item = []
-        # for i in orden.items:
-        #     items.append(i)
-
-        # db.init_app(app)
-        # db.create_all()
-        # with app.app_context():
-        #     item1 = ItemDTO(item='pantalon1')
-        #     item2 = ItemDTO(item='pantalon2')
-        #     item3 = ItemDTO(item='pantalon3')
-
-        #     ord = OrdenDTO(user='usuario1', user_address='call 35 65 43', items=[item1,item2,item3])
-
-        #     db.session.add(ord)
-        #     db.session.add(item1)
-        #     db.session.add(item2)
-        #     db.session.add(item3)
-        #     db.session.commit()
-        #     print(OrdenDTO.query.all())
-        #     print(ItemDTO.query.all())
-        #     print("HHFMTT")
-
-        orden_dto = self.fabrica_ordenes.crear_objeto(orden, MapeadorOrden())
-        
+        item_dto = []
         for item in orden.items:
-            db.session.add(Item(item=item.item))
-        db.session.add(orden_dto)
-        #db.session.flush()
+            item_dt = Item(item=item.item)
+            db.session.add(item_dt)
+            item_dto.append(item_dt)
+
+        ord = Orden(user=orden.user, user_address=orden.user_address, items=item_dto)
+        db.session.add(ord)
+        print("RepositorioOrdenesSQLite obj agregado a DB: ", ord)
+        print("\n")
+
         db.session.commit()
 
     def actualizar(self, orden: Orden):
